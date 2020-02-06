e8f7fdfcf3126adb27691b2df3ee9c646b5a44c6
Stephen March <stephen.march@gmail.com>
2020-02-06 15:57:38 -0500

    updated note - for dotnet image not complete

--- Docker/dotnetcore/create-dotnetcore.sh
+++ Docker/dotnetcore/create-dotnetcore.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#  Wip - not 100% working
+#
 
 # Requires .NET Core installed, creates a sample MVC app in the current directory
 dotnet new mvc
@@ -8,7 +9,8 @@ dotnet new mvc
 docker pull mcr.microsoft.com/dotnet/core/aspnet
 docker pull mcr.microsoft.com/dotnet/core/sdk:3.0
 
-
 # Create a container and put in interactive mode - mapped to current directory where mvc app is
 #docker run -it -p 8080:80 -v $(pwd):/app -w "/app" microsoft/aspnetcore-build /bin/bash
-docker run -it -p 8080:80 -v $(pwd)/app:/app -w "/app" mcr.microsoft.com/dotnet/core/sdk:3.0 /bin/bash
+#docker run -it -p 8080:80 -v $(pwd)/app:/app -w "/app" mcr.microsoft.com/dotnet/core/sdk:3.0 /bin/bash
+
+docker run -d -p 8080:80 -v $(pwd)/app:/app -w "/app" mcr.microsoft.com/dotnet/core/sdk:3.0
